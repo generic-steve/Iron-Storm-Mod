9749d85297a7bee19e84e064c8aae70fa1f1cc2a
generic-steve <44148372+generic-steve@users.noreply.github.com>
2018-10-16 19:39:20 -0500

    Add files via upload

---
+++ src/main/java/com/generic__steve/IronStorm/init/ModItems.java
@@ -0,0 +1,15 @@
+package com.generic__steve.IronStorm.init;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.generic__steve.IronStorm.items.ItemBase;
+
+import net.minecraft.item.Item;
+
+public class ModItems 
+{
+	public static final List<Item> ITEMS = new ArrayList<Item>();
+	public static final Item TITANIUM_INGOT = new ItemBase("titaniumingot");
+	
+}
---
+++ src/main/java/com/generic__steve/IronStorm/util/Reference.java
@@ -0,0 +1,10 @@
+package com.generic__steve.IronStorm.util;
+
+public class Reference {
+	public static final String MOD_ID = "ism";
+	public static final String NAME = "Iron Storm Mod";
+	public static final String Version = "1.0";
+	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+	public static final String CLIENT_PROXY_CLASS = "com.generic__steve.IronStorm.proxy.ClientProxy";
+	public static final String COMMON_PROXY_CLASS = "com.generic__steve.IronStorm.proxy.CommonProxy";
+}
